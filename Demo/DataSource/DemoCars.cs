1def07de0cfadd9be44b5bcae303e8f8431a53a5
Casper Hooft <casperhooft@hotmail.nl>
2020-02-05 17:01:12 +0100

    Added more cars

--- Demo/DataSource/DemoCars.cs
+++ Demo/DataSource/DemoCars.cs
@@ -44,18 +44,54 @@ namespace Demo.DataSource
                     AmountMade = 1,
                     _brand = Brands.Audi,
                     Colour= "red",
-                    CreationTime = DateTime.Now,
+                    CreationTime = DateTime.Today,
                     APK = true
                 },
                 new Car()
                 {
                     ID = 1,
-                    AmountMade = 1,
+                    AmountMade = 1234234,
                     _brand = Brands.BMW,
+                    Colour= "Blue",
+                    CreationTime = DateTime.UtcNow,
+                    APK = false
+                },
+                new Car()
+                {
+                    ID = 2,
+                    AmountMade = 353451,
+                    _brand = Brands.Ferrari,
+                    Colour= "red",
+                    CreationTime = DateTime.Now,
+                    APK = true
+                },
+                new Car()
+                {
+                    ID = 3,
+                    AmountMade = 1678678,
+                    _brand = Brands.Ford,
+                    Colour= "Blue",
+                    CreationTime = DateTime.UtcNow,
+                    APK = false
+                },
+                new Car()
+                {
+                    ID = 4,
+                    AmountMade = 123424,
+                    _brand = Brands.Honda,
+                    Colour= "red",
+                    CreationTime = DateTime.Today,
+                    APK = true
+                },
+                new Car()
+                {
+                    ID = 5,
+                    AmountMade = 1345353,
+                    _brand = Brands.Nissan,
                     Colour= "Blue",
                     CreationTime = DateTime.Now,
                     APK = false
-                }
+                },
             });
         }
     }
