4a9551315767d2abeb1de941ad791e8b5884e92a
Casper Hooft <casperhooft@hotmail.nl>
2020-02-13 09:51:42 +0100

    Added remove functionality to database

--- Demo/Controllers/CarsController.cs
+++ Demo/Controllers/CarsController.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Web.Http;
 using Microsoft.AspNet.OData;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace Demo.Controllers
 {
@@ -28,13 +29,48 @@ namespace Demo.Controllers
             //db.Products.Add(product);
             //await db.SaveChangesAsync();
 
-
             //await DemoCars.Instance.Cars.SaveChangesAsync();
             
             DemoCars.Instance.Cars.Add(car);
 
             return Created(car);
         }
-        
+
+        public async Task<IHttpActionResult> Patch (int key, Car car)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            //Car car = DemoCars.Instance.Cars.Find(key);
+
+            if (key != car.ID)
+            {
+                return BadRequest();
+            }
+
+            DemoCars.Instance.Cars.RemoveAt(key);
+
+            return Updated(car);
+        }
+
+        public async Task<IHttpActionResult> Delete([FromODataUri] int key)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            //var target = DemoCars.Instance.Cars.; 
+            //contactContext.Contacts.Remove(contact);
+            //await contactContext.SaveChangesAsync();
+            DemoCars.Instance.Cars.RemoveAt(key);
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+
+
     }
 }
