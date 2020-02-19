ac4c6f06bffb16dc6f49a475dfc3477814f7b221
Casper Hooft <casperhooft@hotmail.nl>
2020-02-19 13:03:53 +0100

    Custom EDM model works

--- Demo/App_Start/WebApiConfig.cs
+++ Demo/App_Start/WebApiConfig.cs
@@ -14,27 +14,49 @@ namespace Demo
             config.MapODataServiceRoute("odata", null, GetEdmModel(), new DefaultODataBatchHandler(GlobalConfiguration.DefaultServer));
             config.EnsureInitialized();
         }
-        private static IEdmModel GetEdmModel()
+        static IEdmModel GetEdmModel()
         { 
             ODataModelBuilder builder = new ODataModelBuilder();
 
+            //EntitySetConfiguration<Car> cars = builder.EntitySet<Car>("cars");
+            //EntityTypeConfiguration<Car> Car = cars.EntityType;
+            //Car.EnumProperty<_Brands>(c => c.Brand).Name = "Brand";
+
             builder.Namespace = "Demos";
             builder.ContainerName = "DefaultContainer";
+            builder.EntitySet<Car>("Cars");
+            builder.EnumType<_Brands>();
 
             var cars = builder.EntityType<Car>();
+
             cars.HasKey(c => c.ID);
             cars.Property(c => c.AmountMade);
             cars.Property(c => c.APK);
             cars.Property(c => c.Colour);
             cars.Property(c => c.TimeWhenAddedToDatabase);
+            cars.EnumProperty(c => c.Brand);
+
+            //builder.ComplexType<JobParameter>();
+            //builder.ComplexType<JobStatusResult>();
+            //builder.EnumType<JobStatus>();
+            //builder.EnumType<JobParameterType>();
+
+            //cars.EnumProperty(c => c.Brand);
+
+            //cars.EnumProperty<_Brands>(c => c.Brand).Name = "Brand";
+            //cars.EnumProperty(c => c.Brand);
 
             //cars.EnumProperty<_Brands>(c => c.Brand);
+
             //cars.Property<_Brands>(c => c.Brand);
-                       
+
+            //EdmEnumType color = new EdmEnumType("NS", "Color");
+            //color.AddMember(new EdmEnumMember(color, "Red", EdmValue (0)));
+            //model.AddElement(color);
 
             //var people = builder.EntitySet<Person>("People");
             //var trips = builder.EntitySet<Trip>("Trips");
-            builder.EntitySet<Car>("Cars");
+            //
 
             //people.EntityType.Count().Filter().OrderBy().Expand().Select();
             //trips.EntityType.Count().Filter().OrderBy().Expand().Select();
