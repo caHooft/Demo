769d665b1b52963f35939909aab04e08badb3e12
Casper Hooft <casperhooft@hotmail.nl>
2020-02-25 13:38:42 +0100

    Tried a lotta ways to make the code more generic. Also wrote some of the big lines in my Log. Commented some tried actions

--- Demo/App_Start/WebApiConfig.cs
+++ Demo/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
 ﻿using Demo.Models;
+using System;
 using Microsoft.OData.Edm;
 using System.Web.Http;
 using Microsoft.AspNet.OData.Batch;
@@ -15,11 +16,12 @@ namespace Demo
             config.EnsureInitialized();
         }
 
-        public static IEdmModel GetEdmModel()
+        public static IEdmModel GetEdmModel ()
         {
             //use convention version to run without adding problems
             //var builder = new ODataConventionModelBuilder();
             var builder = new ODataModelBuilder();
+            var entity = new EntityTypeConfiguration();
 
             //EntitySetConfiguration<Car> cars = builder.EntitySet<Car>("cars");
             //EntityTypeConfiguration<Car> Car = cars.EntityType;
@@ -28,6 +30,10 @@ namespace Demo
             builder.Namespace = "Demos";
             builder.ContainerName = "DefaultContainer";
             builder.EntitySet<Car>("Cars");
+
+
+            //deze werkt zonder dat je elk type hoeft te declaren maar dan moet je dit in de gegenereerde code doen (client side)
+            //builder.EnumType<_Brands>();
 
             //declare de ENUM en geef de opties (members)
             var _Brands = builder.EnumType<_Brands>();
@@ -44,10 +50,56 @@ namespace Demo
             _Brands.Member(Models._Brands.Toyota);
             _Brands.Member(Models._Brands.Volkswagen);
 
-            //deze werkt zonder dat je elk type hoeft te declaren maar dan moet je dit in de gegenereerde code doen
-            //builder.EnumType<_Brands>();
+            //visual studio gaf aan dat dit beter zou moeten wezen dan AddEntityType echter werkt het niet echter mischien handig even te onderzoeken
+            //EdmEntityType ;
+
+            //getting closer to something that doesnt error
+            //var a = builder.AddEntityType(T);
+            //var a = builder.AddEntityType(Type T);
+
+            //tried to convert it into a method gave reallt wierd errors
+            //public virtual Microsoft.AspNet.OData.Builder.EntityTypeConfiguration AddEntityType(Type type);
+
+            //Type T;
+
+            //var a = builder.AddEntityType(T);
+
+            //doesnt work because its a type that cant be set to a type...
+            //Type a = EntityTypeConfiguration;
+
+            //doesnt work while it give it the exact parameter it wants
+            //EntityTypeConfiguration<TEntituType>;
+
+            //i cant put a type in type configurator?..
+            //var a = EntityTypeConfiguration<Type.EmptyTypes>;
+
+            //if i give it a diffrent variabel it still says its a type...
+            //var a = Microsoft.AspNet.OData.Builder.EntityTypeConfiguration<int a>;
+
+            //var a = 
+
+            //var a = entity.AddProperty(Car);
+            //var a = entity;
+
+            //a.ModelBuilder.AddEntityType(Person);
+
+            //somehow even when litterally saying add entity type it doesnt wanna take a type
+            //var a = entity.ModelBuilder.AddEntityType<Car>;
+
+            //cannot assign method group to an emplicity-typed variabel
+            //var a = entity.ModelBuilder.EntityType<Car>;
+
+            //werkt niet
+            //entity.BaseType{get;}
+
+            //tried out if add entity existed in ODataModelBuilder class and it does
+            //sadly it doesnt take a type which is exactly what i need
+            //builder.AddEntityType(Person);
+
+
 
             var cars = builder.EntityType<Car>();
+
             cars.HasKey(c => c.ID);
             cars.Property(c => c.AmountMade);
             cars.Property(c => c.APK);
@@ -56,7 +108,6 @@ namespace Demo
             cars.EnumProperty(c => c.Brand);
 
             cars.Count().Filter().OrderBy().Expand().Select();
-            //
 
             //people.EntityType.Count().Filter().OrderBy().Expand().Select();
             //trips.EntityType.Count().Filter().OrderBy().Expand().Select();
@@ -65,7 +116,7 @@ namespace Demo
             var edmModel = builder.GetEdmModel();
 
             //return builder.GetEdmModel();
-            return builder.GetEdmModel();
+            return edmModel;
         }
     }
 }
