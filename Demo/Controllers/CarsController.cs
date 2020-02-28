65bdc4fd1e7882db45b733d698ec0b9eb61763e7
Casper Hooft <casperhooft@hotmail.nl>
2020-02-28 16:37:27 +0100

    removed server based time calling and added comments

--- Demo/Controllers/CarsController.cs
+++ Demo/Controllers/CarsController.cs
@@ -23,20 +23,16 @@ namespace Demo.Controllers
 
         public HttpResponseMessage Post(Car car)
         {
+            //Add a car to the database.
             DemoDataSources.Instance.Cars.Add(car);
 
-            //var header = HttpRequestHeader.Accept;
-
-            //return Created(car);
-
-            //return Updated(car);
-
-            //return HttpRequestHeader.Accept;
-
-            //return StatusCode(HttpStatusCode.Created);
+            //Set the responce into the varabel responce. I do this so i can easily debug when something is wrong.
             HttpResponseMessage response = Request.CreateResponse<Car>(HttpStatusCode.Created, car);
 
+            //This is the link to the created item. It shield give a direct route towards the created item.
             string uriToTheCreatedItem = Url.Route(null, new { id = car.ID });
+
+            //Makes a new URI from the uri of the newly create item.
             response.Headers.Location = new Uri(Request.RequestUri, uriToTheCreatedItem);
 
             return response;
@@ -49,22 +45,20 @@ namespace Demo.Controllers
                 return BadRequest(ModelState);
             }
 
+            //If there is no car that has a matching car.ID with the ID given by the client side than stop. Also send a bad request so i can debug if needed.
             if (key != car.ID)
             {
-                car.TimeWhenAddedToDatabase = DateTime.Now;
-
-                DemoDataSources.Instance.Cars.Insert(key, car);
 
                 return BadRequest();
             }
-
+            
+            //If there is a car that has a matching Car.ID with the given key by the client side. Delete the car that is currently in this position than create a new car with the specs given from the client side.
             if (key == car.ID)
             {
-
+                //Remove old entry from the database based on ID.
                 DemoDataSources.Instance.Cars.RemoveAll(r => r.ID == key);
 
-                car.TimeWhenAddedToDatabase = DateTime.Now;
-
+                //Insert new entry into database based on ID.
                 DemoDataSources.Instance.Cars.Insert(key, car);
             }
 
@@ -77,8 +71,9 @@ namespace Demo.Controllers
             {
                 return BadRequest(ModelState);
             }
-
+            //Removes a entry from the database based on ID. I made it remove all in case double ID entries exist.
             DemoDataSources.Instance.Cars.RemoveAll(r => r.ID == key);
+            //Return a no content HttpStatusCode. My choice went to No content because there now is no content at this point in  the database.
             return StatusCode(HttpStatusCode.NoContent);
         }
     }
